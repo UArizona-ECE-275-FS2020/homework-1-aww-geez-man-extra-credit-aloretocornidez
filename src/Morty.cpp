5e8d8c792d311be449606620c081bd2120a1169d
aloretocornidez <70230057+aloretocornidez@users.noreply.github.com>
2020-09-28 14:21:31 -0600

    Add files via upload

--- src/Morty.cpp
+++ src/Morty.cpp
@@ -11,6 +11,7 @@
 #include "Morty.hpp"
 #include <fstream>
 #include <iostream>
+#include <string.h>
 
 // =========================================================================== // 
 //          Define functions enclosed in Namespace interdimensional here 
